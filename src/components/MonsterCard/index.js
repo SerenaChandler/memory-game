4e133326cc2a4654d69d002f54621640f0f10f77
Serena Chandler <serenachandler2019@gmail.com>
2021-05-16 13:33:54 -0700

    added new images, changed cards to use them

--- src/components/MonsterCard/index.js
+++ src/components/MonsterCard/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 function FriendCard(props) {
 
   return (
-    <div  className="card">
+    <div onClick = {props.checkCard} data-clicked={props.clicked} data-id={props.id} className="card">
       <div onClick = {props.checkCard} data-clicked={props.clicked} data-id={props.id} className="img-container">
-        <img  src={props.image} />
+        <img onClick = {props.checkCard} data-clicked={props.clicked} data-id={props.id} src={props.image} />
       </div>
     </div>
   );
