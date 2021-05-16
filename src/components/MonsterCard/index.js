06ce8639d24728824899e4a7802091715ca0e485
Serena Chandler <serenachandler2019@gmail.com>
2021-05-16 13:48:42 -0700

    added and alt tag

--- src/components/MonsterCard/index.js
+++ src/components/MonsterCard/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 function FriendCard(props) {
 
   return (
-    <div onClick = {props.checkCard} data-clicked={props.clicked} data-id={props.id} className="card">
-      <div onClick = {props.checkCard} data-clicked={props.clicked} data-id={props.id} className="img-container">
-        <img onClick = {props.checkCard} data-clicked={props.clicked} data-id={props.id} src={props.image} />
+    <div  className="card">
+      <div  className="img-container">
+        <img alt="a monster from the video game series Monster Hunter" src={props.image} />
       </div>
     </div>
   );
