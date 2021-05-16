db18000557df4ffd38b3299d24925770068bf829
Serena Chandler <serenachandler2019@gmail.com>
2021-05-16 13:06:00 -0700

    updated js for friend card, still needs more work

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 function FriendCard(props) {
 
   return (
-    <div onClick = {props.checkCard} data-clicked={props.clicked} data-id={props.id} className="card">
+    <div  className="card">
       <div onClick = {props.checkCard} data-clicked={props.clicked} data-id={props.id} className="img-container">
-        <img onClick = {props.checkCard} data-clicked={props.clicked} data-id={props.id} src={props.image} />
+        <img  src={props.image} />
       </div>
     </div>
   );
