c5b957aed8c4f0449b1ec05033151ab17a4c6812
Serena Chandler <serenachandler2019@gmail.com>
2021-05-17 09:23:30 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # memory-game
+
+## summary
+A memory game that requires you to not click on the same image more than once.
+
+## Built with
+-React
+
+-Javascript
+
+-CSS
+
+-HTML
+
+## Code Snippet
+
+Code that rendered monster cards and set up props
+```
+  render() {
+    return (
+      <Wrapper>
+        <Navbar
+          message={this.state.message}
+          currentScore={this.state.currentScore}
+          highScore={this.state.highScore}
+        />
+
+        {this.state.monsters.map((friend) => (
+          <MonsterCard
+            name={friend.name}
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+            checkCard={this.checkCard}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+```
+
+This code checks to see if the card has already been clicked. if it has, it ends the game and resets, if it hasn't
+it filters out the clicked card from unclickedMonsters, and increments your score by one, using setState
+```
+  checkCard = (name) => {
+    const checkMonster = this.state.unclickedMonsters.find(
+      (item) => item.name === name
+    );
+
+    if (checkMonster === undefined) {
+      this.setState({
+        highScore:
+          this.state.currentScore > this.state.highScore
+            ? this.state.currentScore
+            : this.state.highScore,
+        currentScore: 0,
+        message: `Oh no, you already clicked on ${name}`,
+        unclickedMonsters: monsters,
+      });
+    } else {
+      const remainingMonsters = this.state.unclickedMonsters.filter(
+        (item) => item.name !== name
+      );
+
+      this.setState({
+        currentScore: this.state.currentScore + 1,
+        message: `Nice, you hadn't clicked on ${name} yet!`,
+        unclickedMonsters: remainingMonsters,
+      });
+      this.shuffleCards(monsters);
+    }
+  };
+  ```
--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import MonsterCard from "./components/MonsterCard";
 import Wrapper from "./components/Wrapper";
 
 import monsters from "./monster.json";
-import Navbar from "./components/Navbar/Navbar"
-
-
+import Navbar from "./components/Navbar/Navbar";
 
 class App extends Component {
   // Setting this.state.monsters to the monsters json array
@@ -15,60 +13,61 @@ class App extends Component {
     currentScore: 0,
     monsters: monsters,
     clickedMonsters: monsters,
-    unclickedMonsters: monsters
+    unclickedMonsters: monsters,
   };
 
-  shuffleCards = array => {
+  shuffleCards = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * ( i + 1));
+      let j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
-    };
-  }; 
-
-  checkCard = (name) => {
-
-    const checkMonster = this.state.unclickedMonsters.find(item => item.name === name);
-    
-    if (checkMonster === undefined) {
-      this.setState({
-        highScore: (this.state.currentScore > this.state.highScore) ? this.state.currentScore : this.state.highScore,
-        currentScore: 0,
-        message: `Oh no, you already clicked on ${name}`,
-        unclickedMonsters: monsters
-      })
-     
-     
-    }else {
-
-      const remainingMonsters = this.state.unclickedMonsters.filter(item => item.name !== name)
-
-      this.setState({
-        currentScore: this.state.currentScore + 1,
-        message:`Nice, you hadn't clicked on ${name} yet!`,
-        unclickedMonsters: remainingMonsters
-      })
-      this.shuffleCards(monsters)
-
     }
   };
 
-  // Map over this.state.monsters and render a FriendCard component for each friend object
+  checkCard = (name) => {
+    const checkMonster = this.state.unclickedMonsters.find(
+      (item) => item.name === name
+    );
+
+    if (checkMonster === undefined) {
+      this.setState({
+        highScore:
+          this.state.currentScore > this.state.highScore
+            ? this.state.currentScore
+            : this.state.highScore,
+        currentScore: 0,
+        message: `Oh no, you already clicked on ${name}`,
+        unclickedMonsters: monsters,
+      });
+    } else {
+      const remainingMonsters = this.state.unclickedMonsters.filter(
+        (item) => item.name !== name
+      );
+
+      this.setState({
+        currentScore: this.state.currentScore + 1,
+        message: `Nice, you hadn't clicked on ${name} yet!`,
+        unclickedMonsters: remainingMonsters,
+      });
+      this.shuffleCards(monsters);
+    }
+  };
+
+
   render() {
     return (
       <Wrapper>
-            <Navbar
-          message = {this.state.message}
-          currentScore = {this.state.currentScore}
-          highScore = {this.state.highScore}
+        <Navbar
+          message={this.state.message}
+          currentScore={this.state.currentScore}
+          highScore={this.state.highScore}
         />
-        
+
         {this.state.monsters.map((friend) => (
           <MonsterCard
             name={friend.name}
             id={friend.id}
             key={friend.id}
             image={friend.image}
-            // clicked={friend.clicked}
             checkCard={this.checkCard}
           />
         ))}
